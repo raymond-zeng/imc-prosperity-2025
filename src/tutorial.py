fa85a2488126fee0ca27a3c4ad3003c03ab88fa4
rahulvg257 <159657631+rahulvg257@users.noreply.github.com>
2025-04-04 01:34:04 -0400

    Add Preliminary Resin Trader

--- src/tutorial.py
+++ src/tutorial.py
@@ -20,28 +20,48 @@ class Strategy:
     def buy(self, state: TradingState, price: int, quantity: int) -> None:
         self.orders.append(Order(self.symbol, price, quantity))
 
-    def buy(self, state: TradingState, price: int, quantity: int) -> None:
+    def sell(self, state: TradingState, price: int, quantity: int) -> None:
         self.orders.append(Order(self.symbol, price, -quantity))
 
 class MarketMakingStrategy(Strategy):
 
-    def get_true_value(state: TradingState) -> int:
+    def get_true_value(self, state: TradingState) -> int:
         raise NotImplementedError
     
     def act(self, state: TradingState) -> None:
+
+        # Get True Value of Asset
         true_value = self.get_true_value(state)
 
+        # Get Buy and Sell Orders
         order_depth = state.order_depths[self.symbol]
         buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
         sell_orders = sorted(order_depth.sell_orders.items())
 
+        # Get buy and sell limits
         position = state.position.get(self.symbol, 0)
         to_buy = self.limit - position
         to_sell = self.limit + position
 
+        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
+        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
+
+        for price, volume in sell_orders:
+            if to_buy > 0 and price <= max_buy_price:
+                quantity = min(to_buy, -volume)
+                self.buy(state, price, quantity)
+                to_buy -= quantity
+
+        for price, volume in buy_orders:
+            if to_sell > 0 and price >= min_sell_price:
+                quantity = min(to_sell, volume)
+                self.sell(state, price, quantity)
+                to_sell -= quantity
+        
+
 class ResinStrategy(MarketMakingStrategy):
 
-    def get_true_value(state: TradingState) -> int:
+    def get_true_value(self, state: TradingState) -> int:
         return 10_000
 
 class Trader:
@@ -52,8 +72,20 @@ class Trader:
             "KELP" : 50
         }
 
+        self.strategies = {symbol: constructor(symbol, limits[symbol]) for symbol, constructor in {
+            "RAINFOREST_RESIN" : ResinStrategy,
+        }.items()}
+
     def run(self, state : TradingState) -> tuple[dict[Symbol, list[Order]], int , str]:
-        old_trading_data = {}
+        conversions = 0
+        trading_data = {}
         if state.traderData != None and state.traderData != "":
-            old_trading_data = jsonpickle.decode(state.traderData)
+            trading_data = jsonpickle.decode(state.traderData)
+        
         orders = {}
+        for symbol, strategy in self.strategies.items():
+            if symbol in state.order_depths:
+                orders[symbol] = strategy.run(state)
+
+        trader_data = jsonpickle.encode(trading_data)
+        return orders, conversions, trader_data
